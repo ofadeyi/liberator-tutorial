2dcb36f0684468619f1e43920b0c73d577e4c338
ofadeyi <ofadeyi@gmail.com>
2017-02-19 23:30:49 +0000

    LIB-004 | Ola | Added “passing” sample test file

---
+++ test/liberator_tutorial/core_test.clj
@@ -0,0 +1,7 @@
+(ns liberator-tutorial.core-test
+  (:require [clojure.test :refer :all]
+            [liberator-tutorial.core :refer :all]))
+
+(deftest a-test
+  (testing "1 equals 1."
+    (is (= 1 1))))
